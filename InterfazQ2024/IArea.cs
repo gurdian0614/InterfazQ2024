e68a46e3b2a23d9ce3f077a9aa192f7bf2b87624
gurdian0614 <ivan.gurdian@outlook.com>
2024-05-03 18:38:44 -0600

    Crando interfaz area

---
+++ InterfazQ2024/IArea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfazQ2024
+{
+    public interface IArea
+    {
+        /// <summary>
+        /// Implementa el proceso de calcular el área del cuadrado
+        /// </summary>
+        /// <returns>Area del cuadrado</returns>
+        public double Cuadrado();
+
+        /// <summary>
+        /// Implementa el proceso de calcular el área del triángulo
+        /// </summary>
+        /// <returns>Area del triángulo</returns>
+        public double Triangulo();
+    }
+}
